c8495b64c84695ec097309c807832912d58eb8e9
Shreekant Suhas Pukale <111984571+Shreekant16@users.noreply.github.com>
2022-09-30 13:15:06 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+import smtplib
+
+import pandas
+from datetime import datetime
+
+today_month = datetime.now().month
+today_date = datetime.now().day
+data = pandas.read_csv('data.csv')
+month = data.month.to_list()
+my_mail = "shreekantpukale16@gmail.com"
+password = "odrnblbwytixvppi"
+if today_month in month:
+    row_data = data[data.month == today_month]
+    if int(today_date) == int(row_data.date):
+        connection = smtplib.SMTP('smtp.gmail.com')
+        connection.starttls()
+        connection.login(user=my_mail, password=password)
+        connection.sendmail(from_addr=my_mail, to_addrs=row_data.gmail, msg=f"SUBJECT: HAPPY BIRTHDAY\n\n"
+                                                                           f"Have a lot of cake but don't be diabetic "
+                                                                           f"From your loving Shreekant")
+        connection.close()
